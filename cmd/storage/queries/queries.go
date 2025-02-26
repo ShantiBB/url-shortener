d1407e4cc73b0da98fbca3d415dd670a21db8e29
Сергей Голобоков <sergeygolobokov@gmail.com>
2025-02-26 23:57:53 +0300

    Add sql queries

---
+++ cmd/storage/queries/queries.go
@@ -0,0 +1,12 @@
+package queries
+
+const CreateURLTable string = `
+	CREATE TABLE IF NOT EXISTS url (
+		id INTEGER PRIMARY KEY,
+		alias TEXT NOT NULL UNIQUE,
+		url TEXT NOT NULL
+	);
+	CREATE INDEX IF NOT EXISTS idx_alias ON url (alias);
+`
+
+const CreateURL string = `INSERT INTO url(url, alias) VALUES(?, ?)`
