c97473a29dacf85aa713f97bd938b2723ab1b075
Сергей Голобоков <sergeygolobokov@gmail.com>
2025-04-12 00:25:17 +0300

    Move setup logger from main

--- cmd/internal/lib/logger/slog/slog.go
+++ cmd/internal/lib/logger/slog/slog.go
@@ -2,9 +2,45 @@ package sl
 
 import (
 	"log/slog"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func SetupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(
+				os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo},
+			),
+		)
+	}
+	return log
+}
 
 func Err(err error) slog.Attr {
 	return slog.Attr{
