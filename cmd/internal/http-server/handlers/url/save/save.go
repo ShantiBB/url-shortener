b07e20e0db599e447146d86832c4d7d2d773146e
Сергей Голобоков <sergeygolobokov@gmail.com>
2025-04-12 16:26:10 +0300

    Add save handler

---
+++ cmd/internal/http-server/handlers/url/save/save.go
@@ -0,0 +1,89 @@
+package save
+
+import (
+	"errors"
+	"log/slog"
+	"net/http"
+	"url-shortener/cmd/internal/storage"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
+	"github.com/go-playground/validator/v10"
+
+	resp "url-shortener/cmd/internal/lib/api/response"
+	sl "url-shortener/cmd/internal/lib/logger/slog"
+	"url-shortener/cmd/internal/lib/random"
+)
+
+const aliasLength = 6
+
+type Request struct {
+	URL   string `json:"url" validate:"required,url"`
+	Alias string `json:"alias,omitempty"`
+}
+
+type Response struct {
+	resp.Response
+	Alias string `json:"alias,omitempty"`
+}
+
+type URLSaver interface {
+	SaveURL(url, alias string) error
+}
+
+func New(log *slog.Logger, urlSaver URLSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.url.save.New"
+		getReqID := middleware.GetReqID(r.Context())
+
+		log = log.With(slog.String("op", op), slog.String("request_id", getReqID))
+
+		var req Request
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err = validator.New().Struct(req); err != nil {
+			var validateErr validator.ValidationErrors
+			errors.As(err, &validateErr)
+
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+			render.JSON(w, r, resp.ValidationError(validateErr))
+
+			return
+		}
+
+		alias := req.Alias
+		if alias == "" {
+			alias = random.NewRandomAlias(aliasLength)
+		}
+
+		err = urlSaver.SaveURL(req.URL, alias)
+		if errors.Is(err, storage.ErrURLExists) {
+			render.JSON(w, r, resp.Error("url already exists"))
+
+			return
+		}
+
+		if err != nil {
+			render.JSON(w, r, resp.Error("failed to save URL"))
+
+			return
+		}
+
+		render.JSON(w, r, Response{
+			Response: resp.OK(),
+			Alias:    alias,
+		})
+	}
+}
